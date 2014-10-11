0c06272db6590a75ba2ba199610aa9d03d907de4
Thomas Wilburn <news@thomaswilburn.net>
2014-10-10 18:57:37 -0700

    Fix bug on small mobile viewports (I hope)

--- src/js/hotspot-map.js
+++ src/js/hotspot-map.js
@@ -14,14 +14,24 @@ define([
         src: "@"
       },
       link: function(scope, element, attr) {
-        element.on("click mouseenter", function(e) {
+        element.on("click", function(e) {
           var target = angular.element(e.target);
           var shown = this.querySelector(".show");
           if (shown) {
             angular.element(shown).removeClass("show");
           }
+          document.body.style.minHeight = null;
           if (target.hasClass("spot")) {
             target.addClass("show");
+            //this is a workaround for Pym sizes, and should not be used in standalone pages
+            setTimeout(function() {
+              var popup = e.target.querySelector(".popup");
+              var popupBounds = popup.getBoundingClientRect();
+              var bodyBounds = document.body.getBoundingClientRect();
+              if (popupBounds.bottom > bodyBounds.height) {
+                document.body.style.minHeight = popupBounds.bottom + "px"
+              }
+            })
           }
         });
       }
